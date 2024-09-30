58e1bef44cd6dcae0f1b5c522fc737ff82fa73b7
Ifrahim <147778089+IfrahimA@users.noreply.github.com>
2024-09-30 19:11:23 -0400

    added component to app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Carousel from './components/Carousel';
 import Navbar from './components/NavBar';
 import Footer from './components/Footer';
+import Services from './components/Services';
 
 function App() {
 	return (
 		<>
 			<Navbar />
 			<Carousel />
+			<Services />
 			<Footer />
 		</>
 	);
