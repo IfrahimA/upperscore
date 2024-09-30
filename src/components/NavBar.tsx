2b3ec71bf078134ef10aa066af5263449eaf300b
Ifrahim <147778089+IfrahimA@users.noreply.github.com>
2024-09-29 22:55:10 -0400

    added a nav bar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,35 @@
+/* 
+	Function: Component
+	Returns
+*/
+
+const Navbar = () => {
+	return (
+		<nav className='sticky top-0 flex justify-around items-center pt-8'>
+			<div className='title text-2xl'>
+				<strong>A+</strong> Parh
+			</div>
+			<ul className='flex gap-12'>
+				<li>
+					<a href='#home'>Home</a>
+				</li>
+				<li>
+					<a href='#about'>About</a>
+				</li>
+				<li>
+					<a href='#services'>Services</a>
+				</li>
+				<li>
+					<a href='#newsletter'>Newsletter</a>
+				</li>
+			</ul>
+			<div className="flex gap-8">
+				<button className='outline p-2 rounded-md hover:bg-black hover:text-white transition-all'>
+					Log in
+				</button>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
