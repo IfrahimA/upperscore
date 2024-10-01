7ff067bfd3355783802f75e4d40b6bdd37914b38
andrew <103297057+APresta19@users.noreply.github.com>
2024-09-30 20:06:29 -0400

    added the welcome component to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import Carousel from './components/Carousel';
 import Navbar from './components/NavBar';
 import Footer from './components/Footer';
 import Services from './components/Services';
+import Welcome from './components/Welcome';
 
 function App() {
 	return (
 		<>
 			<Navbar />
+			<Welcome />
 			<Carousel />
 			<Services />
 			<Footer />
