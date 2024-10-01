e5a64077a04f4c01adc03a8052478712f7be9cb3
andrew <103297057+APresta19@users.noreply.github.com>
2024-09-30 21:56:55 -0400

    added goals component to home page

---
+++ src/components/Goals.tsx
@@ -0,0 +1,18 @@
+/*
+    function: creates a goals component for the home page
+	returns: component
+*/
+
+const Goals = () => {
+    return (
+        <div className="flex justify-center text-center mb-12">
+            <div className="p-40 pl-96 bg-slate-600 rounded-lg border-2 border-slate-950"></div>
+            <div className="p-20">
+                <h1 className="text-2xl font-semibold">Building the future since 2024</h1>
+                <h3 className="text-lg font-light">Our plan is to have the plan with the goal and the future since it is the goal plan future.</h3>
+            </div>
+        </div>
+    );
+}
+
+export default Goals;
